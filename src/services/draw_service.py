363a82eaf8aa8a4fbbc8afe7ecba4329350ddc96
Michael Frank <maickomtutos@gmail.com>
2022-07-03 01:38:21 -0300

    make a balance teams

--- src/services/draw_service.py
+++ src/services/draw_service.py
@@ -3,6 +3,7 @@ from typing import List
 from fastapi import HTTPException
 
 from src.models.draw_model import DrawInput
+from src.models.player_model import PlayerOutput
 from src.services.team_service import TeamService
 
 
@@ -11,18 +12,25 @@ class DrawService:
         self.teamService = TeamService()
 
     async def draw_players(self, input: DrawInput) -> List:
+        if input.balance_teams:
+            return await self.draw_players_premium(input)
         teams = []
         players = input.players
+        position_name = 'Goleiro'.upper()
         shuffle(players)
         if input.number_of_teams <= 0:
             raise HTTPException(status_code=400, detail="Number of times needs to be greater than 0")
         if (len(players) % input.number_of_teams) > 0:
             raise HTTPException(status_code=400, detail="A number of player pairs is required.")
         players_per_team = len(players) // input.number_of_teams
-        # if players_per_team >= players_per_team:
-        #    raise HTTPException(status_code=400, detail="It is not possible to draw players. number of low players")
+        goalkeepers = list(filter(lambda player: player.position.name.upper() == position_name, players))
+
+        if len(goalkeepers) == input.number_of_teams:
+            players = [item for item in players if not item.position.name.upper() == position_name]
+            players_per_team -= len(goalkeepers)
         team = []
         index = 0
+
         while len(teams) < input.number_of_teams:
             if len(team) >= players_per_team:
                 teams.append({"team": team})
@@ -30,4 +38,46 @@ class DrawService:
             else:
                 team.append(players[index])
                 index = index + 1
+        for number in range(input.number_of_teams):
+            teams[number]["team"].append(goalkeepers[number])
+        return {"teams": teams}
+
+    @staticmethod
+    def getPerformance(player: PlayerOutput):
+        total = (player.skills.strength +
+                 player.skills.velocity +
+                 player.skills.completion +
+                 player.skills.dribble)
+        return total
+
+    async def draw_players_premium(self, input: DrawInput) -> List:
+        teams = []
+        players = input.players
+        position_name = 'Goleiro'.upper()
+        shuffle(players)
+        if input.number_of_teams <= 0:
+            raise HTTPException(status_code=400, detail="Number of times needs to be greater than 0")
+        if (len(players) % input.number_of_teams) > 0:
+            raise HTTPException(status_code=400, detail="A number of player pairs is required.")
+        players_per_team = len(players) // input.number_of_teams
+        goalkeepers = list(filter(lambda player: player.position.name.upper() == position_name, players))
+
+        if len(goalkeepers) == input.number_of_teams:
+            players = [item for item in players if not item.position.name.upper() == position_name]
+            players_per_team -= len(goalkeepers)
+        players.sort(reverse=True, key=self.getPerformance)
+
+        for index in range(input.number_of_teams):
+            teams.append({"team": []})
+
+        index = 1
+        for player in players:
+            teams[(index-1)]["team"].append(player)
+            if index >= input.number_of_teams:
+                index = 1
+            else:
+                index += 1
+
+        for number in range(input.number_of_teams):
+            teams[number]["team"].append(goalkeepers[number])
         return {"teams": teams}
